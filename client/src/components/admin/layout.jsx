6539ca14215fc37b4fc8c83100e28104fe2e3606
Ayman ezz <aymanmae12@gmail.com>
2024-11-04 18:26:33 +0200

    update nav

--- client/src/components/admin/layout.jsx
+++ client/src/components/admin/layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 const AdminLayout = () => {
   return (
-    <div className="flex min-h-screen w-full" >
+    <div className="flex min-h-screen container mx-auto " >
       <div className="flex flex-1 flex-col" >
         <main className=" flex-1 flex-col px-4 md:p-6" >
           <Outlet />
